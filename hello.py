48098225a8fcdccc9798467daca0f1180c4284ad
Dionysis Fachiridis <78432513+fachid@users.noreply.github.com>
2021-10-06 16:34:46 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
 from flask import Flask, request
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    print request.headers
+    print (request.headers)
     return "Hello World!"
+
 
 if __name__ == "__main__":
     app.run()
-
-    
