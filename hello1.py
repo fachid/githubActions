b92dcd9f8339bfa12c8f649cad2099f1af160dc8
Dionysis Fachiridis <78432513+fachid@users.noreply.github.com>
2022-06-21 11:08:17 +0300

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def hello():
     print(request.headers)
     print(request.headers)
+    
+    aws_token = "aaeea383-d591-4640-89b7-5e7c798878bd"
+    
+    vessel_secret="asdlkjadkasd"
+    
     return "Hello World!!!!"
 
 if __name__ == "__main__":
